937112b4def4c640324a493ded52cdaf8dfa8747
Yollam Chilenje <94620838+yolly265@users.noreply.github.com>
2024-04-23 16:00:39 +0200

    filtered to those 30 and above

--- main.R
+++ main.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(here)
 
 tv_hours_table <- gss_cat %>% 
+  filter(age >= 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T)) 
 
 write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
+
